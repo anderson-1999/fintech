0d6140013b018d588237162cc702b75e7e782e76
Anderson de Almeida <anderson.silvadealmeida1999@gmail.com>
2023-06-27 18:06:37 -0300

    Atualizando ajuste de valor

---
+++ assets/js/ajustarValor.js
@@ -0,0 +1,11 @@
+//recuperando o atributo na página HTML
+const saldo = document.querySelector('[saldo]');
+
+//imprimimdo valor do atributo
+console.log(saldo.textContent);
+
+//alterando valor do atributo
+setTimeout(function() {
+    saldo.textContent = '3,00';
+    console.log(saldo.textContent);
+}, 5000);
